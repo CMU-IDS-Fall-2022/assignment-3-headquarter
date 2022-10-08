359687e1af565f72518827299d951f360d5cb486
Hazeland-hx <hazelhxiaozhang@outlook.com>
2022-10-08 14:08:17 -0400

    YelpData

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,24 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import json
+# from vega_datasets import data
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Let's analyze some Food Data in Philadaphia.")
 
 @st.cache  # add caching so we load the data only once
+
 def load_data():
-    # Load the penguin data from https://github.com/allisonhorst/palmerpenguins.
-    penguins_url = "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/v0.1.0/inst/extdata/penguins.csv"
-    return pd.read_csv(penguins_url)
+    # Load the business data
+    fin = 'yelp_academic_dataset_business.json'
+    raw_data = open (fin).readlines()
 
 df = load_data()
+
+# print ('Number of rows in the file: %d' % (len(raw_data)))
+# print (raw_data[1])
+
+
 
 st.write("Let's look at raw data in the Pandas Data Frame.")
 
@@ -18,14 +26,17 @@ st.write(df)
 
 st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
 
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-    y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-    color=alt.Y("species")
-).properties(
-    width=600, height=400
-).interactive()
 
-st.write(chart)
 
-st.markdown("This project was created by Student1 and Student2 for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+
+# chart = alt.Chart(df).mark_point().encode(
+#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
+#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+#     color=alt.Y("species")
+# ).properties(
+#     width=600, height=400
+# ).interactive()
+
+# st.write(chart)
+
+st.markdown("This project was created by Hazel Zhang and Qianru Zhang for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
