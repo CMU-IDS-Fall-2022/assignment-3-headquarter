4cce0b2fd9c2173a49e924c47aeb04c3aa7c7995
Hazeland-hx <hazelhxiaozhang@outlook.com>
2022-10-10 22:11:43 -0400

    Wirteup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,195 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import altair as alt
-# import json
-# #from vega_datasets import data
-# import geopandas as gpd
-
-# st.title("Let's analyze some Food Data in Philadaphia.")
-
-# ##################################################################################
-# ##################################################################################
-
-# st.header("1. Load Data")
-
-# def check_json_fmt(line):
-#     # This function check if the row has a valid json formate
-#     # param: str: the row of record read from text file
-#     # return: JSON object if parsed successfully, or None
-#     data = None
-#     try:
-#         data = json.loads(line)
-#     except:
-#         data = None
-#         print ("Fail: %s" % (line))
-#     return data
-
-# @st.experimental_memo
-# def load_data(file):
-#     raw_data = open(file).readlines()
-#     print ('Number of rows in the file: %d' % (len(raw_data)))
-#     filter_data = []
-#     for line in raw_data:
-#         data = check_json_fmt(line)
-#         if data != None:
-#             filter_data.append(data)        
-#     df = pd.json_normalize(filter_data)
-#     #df.info(verbose=True)
-#     return df
-
-# fin = 'yelp_academic_dataset_business.json'
-# df= load_data(fin)
-
-# st.text("Let's look at raw data in the Pandas Data Frame.")
-
-# st.dataframe(df)
-
-# ##################################################################################
-# ##################################################################################
-
-# st.header("2. Data ETL")
-
-# st.text("Filter by City: Philadelphia \n Filter by Category: contains the key word: [Restaurants, Food] \n Filter by Column \n Drop Duplicate \n Handle Missing Data")
-
-# def convert_categorary_column(row):
-#     '''
-#     This function convert the string of categories to a list
-#     '''
-#     row = row.split(',')
-#     row = [s.strip() for s in row]
-#     return row
-
-# def create_is_restaurant_column(row):
-#     '''   
-#     This function filter the rows with key word: 'Restaurants' or 'Food' 
-#     '''
-#     if ('Restaurants' in row) or ('Food' in row):
-#         return True
-#     else:
-#         return False
-
-# def handle_missing(df, handle):
-#     """
-#     This function handle the missing data
-#     """
-#     for colName in handle:
-#         print ("==== column: %s ====" % (colName))
-#         if handle[colName] == 'del':
-#             df.dropna(subset=[colName], inplace=True)
-#             print ("  -- The missing row is deleted")
-#         else:
-#             df[colName].fillna(handle[colName], inplace=True)
-#             print ("  -- The missing row is filled with %s" % ("Unknown"))
-#     return df
-
-# @st.experimental_memo
-# def data_clean(df):
-#     # Keep the Philidaphia data
-#     newDf = df[df['city']=='Philadelphia']
-
-#     # Filter by Category
-#     newDf.dropna(subset = ['categories'], inplace=True)
-#     newDf['category_list'] = newDf['categories'].apply(convert_categorary_column)
-#     newDf['is_restaurant'] = newDf['category_list'].apply(create_is_restaurant_column)
-#     newDf = newDf[newDf['is_restaurant']==True]
-
-#     # Filter by Column
-#     COLUMNS = ['business_id', 'name', 'address', 'city', 'state',  
-#              'latitude', 'longitude', 'stars', 'review_count', 'category_list', 
-#              'attributes.RestaurantsPriceRange2', 'attributes.RestaurantsTakeOut', 
-#              'attributes.RestaurantsDelivery', 'attributes.WheelchairAccessible']
-#     newDf = newDf[COLUMNS]
-
-#     # Drop Duplicate
-#     newDf.drop_duplicates(subset=['business_id'], keep='first', inplace=False)
-
-#     NEW_COL_NAME = ['id', 'name', 'address', 'city', 'state', 'latitude', 'longitude',
-#                     'stars', 'review_count', 'categories', 'price_range', 'takeout',
-#                     'delivery', 'wheelchair_access']
-#     newDf.columns = NEW_COL_NAME
-#     newDf = newDf.drop(['id'], axis=1)
-
-#     # Handle Missing Data
-#     MISS_HANDLE = {
-#         'name': 'del', 
-#         'address': 'del', 
-#         'city': 'del', 
-#         'state': 'del', 
-#         'latitude': 'del', 
-#         'longitude': 'del', 
-#         'stars': 'del', 
-#         'review_count': 'del', 
-#         'categories': 'del',
-#         'price_range': -1, 
-#         'takeout': 'Unknown', 
-#         'delivery': 'Unknown', 
-#         'wheelchair_access': 'Unknown'
-#     }
-#     newDf = handle_missing(newDf, MISS_HANDLE)
-#     return newDf
-
-# newDf = data_clean(df)
-
-# st.dataframe(newDf)
-
-# # st.map(newDf)
-
-
-# ################################
-# #Version one: Background map + interactive points
-
-
-# ##US states background
-# #counties = alt.topo_feature(data.us_10m.url, feature='counties')
-# #background = alt.Chart(counties).mark_geoshape(
-# #    fill='lightgray',
-# #    stroke='white'
-# #).properties(
-# #    width=500,
-# #    height=300
-# #).project('albersUsa')
-
-# gdf = gpd.read_file('./PhillyPlanning_Neighborhoods/PhillyPlanning_Neighborhoods.shp')
-# #Creating an altair map layer
-# choro = alt.Chart(gdf).mark_geoshape(
-#     stroke='black'
-# ).properties(
-#     width=500,
-#     height=300
-# )
-
-# # .encode(
-# #     color=color
-# #    tooltip=['state','count']
-
-# # click = alt.selection_multi(encodings=['color'])
-
-# # # restaurant positions on background
-# # points = alt.Chart(newDf).mark_circle().encode(
-# #     longitude='longitude:Q',
-# #     latitude='latitude:Q',
-# #     color=alt.value('steelblue'),
-# #     tooltip=['name:N']
-# # ).properties(
-# #     title='Restaurants in Phili'
-# # ).transform_filter(
-# #     click
-# # )
-
-# # hist = alt.Chart(newDf).mark_bar().encode(
-# #     x='count()',
-# #     y='stars',
-# #     color=alt.condition(click, 'stars', alt.value('lightgray'))
-# # ).add_selection(
-# #     click
-# # )
-
-# st.altair_chart(choro)
-
-
-# st.markdown("This project was created by Hazel Zhang and Qianru Zhang for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
-
-
 import pandas as pd
 from pandas.io.json import json_normalize
 import json
@@ -217,15 +25,12 @@ def check_json_fmt(line):
 @st.experimental_memo
 def load_data(file):
     raw_data = open(file, encoding="utf-8").readlines()
-    #print ('Number of rows in the file: %d' % (len(raw_data)))
     filter_data = []
     for line in raw_data:
         data = check_json_fmt(line)
         if data != None:
             filter_data.append(data)        
     df = json_normalize(filter_data)
-    #df = pd.json_normalize(filter_data)
-    #df.info(verbose=True)
     return df
 
 ###############################################################################
@@ -363,16 +168,6 @@ def select_data_by_filter(df, categorySelected):
     This function help to filter the data by user selected filters
     """
     newDf = df.copy()
-
-    # if isDelivery:
-    #     newDf = newDf[newDf['delivery']=='True']
-    # if isTakeout:
-    #     newDf = newDf[newDf['takeout']=='True']
-    # if isAccess:
-    #     newDf = newDf[newDf['wheelchair_access']=='True']
-    
-    # if priceRange != 'ALL':
-    #     newDf = newDf[(newDf['price_range']<=int(priceRange)) & (newDf['price_range']>0)]
     
     newDf = filter_by_categoary(newDf, categorySelected)
     return newDf
@@ -398,41 +193,6 @@ def get_name_address(df):
 #       4. Altair Plots
 ###############################################################################
 
-# def plot_scatter(df):
-#     """
-#     This function plot the review count and star scatter for the selected restaurants
-#     """
-#     #df = df.explode('categories').reset_index(drop=True)
-#     scatter = alt.Chart(df).mark_point().encode(
-#         alt.X("review_count", scale=alt.Scale(zero=False), title="Review Count"),
-#         alt.Y("stars", scale=alt.Scale(zero=False), title="Star")
-#     )
-#     return scatter
-
-
-# def plot_histogram(df):
-#     """
-#     This function plot the star histogram for the selected restaurants
-#     """
-#     #df = df.explode('categories').reset_index(drop=True)
-#     hist = alt.Chart(df).mark_bar().encode(
-#         alt.X("stars", bin=alt.Bin(maxbins=5), title="Star"),
-#         alt.Y("count()", title="The number of stars")
-#     )
-#     return hist
-
-
-# def plot_rule(df):
-#     """
-#     This function plot the average restartaurant star
-#     """
-#     #df = df.explode('categories').reset_index(drop=True)
-#     rule = alt.Chart(df).mark_rule().encode(
-#         alt.Y("stars", title="Star"),
-#         alt.Color("categories")
-#     )
-#     return rule
-
 def plot_binnedscatter(df):
 	binned_scatter = alt.Chart(df).mark_circle().encode(
     	alt.X('stars', bin=True, title="Star"),
@@ -456,29 +216,19 @@ def plot_altair(df, categorySelected):
     """
     df = df.explode('categories').reset_index(drop=True)
     df = df[df['categories'].isin(categorySelected)]
-    # scatter = plot_scatter(df)
-    # hist = plot_histogram(df)
-    # rule = plot_rule(df)
 
-    # selection = alt.selection_interval()
-    # fig = scatter.add_selection(selection).encode(
-    #     color=alt.condition(selection, "categories", alt.value("grey"))
-    # ) + rule | hist.encode(
-    #     alt.Color("categories")
-    # ).transform_filter(selection)
-
-    single = alt.selection_interval()
+    selection = alt.selection_interval()
 
     binned_scatter = plot_binnedscatter(df)
     hist = plot_hist(df)
 
-    binned_scatter = binned_scatter.add_selection(single).encode(
-        color=alt.condition(single, "stars", alt.value("grey"))
+    binned_scatter = binned_scatter.add_selection(selection).encode(
+        color=alt.condition(selection, "stars", alt.value("grey"))
     ) 
 
     hist = hist.encode(
         alt.Color("categories")
-    ).transform_filter(single)
+    ).transform_filter(selection)
 
     return alt.vconcat(binned_scatter, hist)
 
@@ -510,7 +260,7 @@ newDf = data_clean(df, SELECTED_CATEGOARIES)
 
 def create_map_figure(df):
     '''
-    This function plot the map of Philidaphia and the restaurant on top of it
+    This function plot the map of Philidelphia and the restaurant on top of it
     PARAM:
         df:: [dataframe] the list of the restaurant that pass the filter
     '''
@@ -523,7 +273,7 @@ def create_map_figure(df):
     for lat, lng, name, address, star, price in zip(latList, lngList, nameList, addressList, starList, priceList):
         feature_group.add_child(folium.Marker(location=[lat,lng],
                                               tooltip=""" 
-                                              <i>Name: </i> <br> <b>{}</b> <br> 
+                                                <i>Name: </i> <br> <b>{}</b> <br> 
                                                 <i>Address: </i><b><br>{}</b><br>
                                                 <i>Stars: </i><b><br>{}</b><br>
                                                 <i>Price Range: </i><b><br>{}</b><br>
@@ -535,7 +285,7 @@ def create_map_figure(df):
 ##################################################################################
 ##################################################################################
 
-st.title("Let's analyze some Food Data in Philadaphia.")
+st.title("Let's analyze some Food Data in Philadelphia.")
 with st.sidebar:
     sideChoice = st.radio(
         "Choose a page", 
@@ -560,12 +310,6 @@ elif sideChoice == "Data ETL":
 ##################################################################################
 elif sideChoice == "Food Map":
     st.header("Food Map")
-    # st.subheader("Restaurant filters")
-    # isDelivery = st.checkbox('Delivery', key='delivery')
-    # isTakeout = st.checkbox('Takeout', key='takeout')
-    # isAccess = st.checkbox('Wheelchair Access', key='accessibility')
-    # priceRange = st.select_slider('Pick a price range', ['1', '2', '3', '4', 'ALL'])
-
     st.subheader("Restaurant categoaries")
     categorySelected = st.multiselect('Categories', SELECTED_CATEGOARIES)
 
@@ -585,11 +329,6 @@ elif sideChoice == "Food Map":
     st.subheader("Restaurant reviews and stars")
     alt_fig = plot_altair(selectDf, categorySelected)
     st.altair_chart(alt_fig)
-
-    # # Show the address and name of the restaurants
-    # st.subheader("Restaurant names and addresses")
-    # dfNameAddress = get_name_address(selectDf)
-    # st.dataframe(dfNameAddress, use_container_width=True)
 
 ##################################################################################
 ##################################################################################
--- writeup.md
+++ writeup.md
@@ -8,16 +8,50 @@ TODO: Short abstract describing the main goals and how you achieved them.
 
 ## Project Goals
 
-TODO: **A clear description of the goals of your project.** Describe the question that you are enabling a user to answer. The question should be compelling and the solution should be focused on helping users achieve their goals. 
+TODO: **A clear description of the goals of your project.** Describe the question that you are enabling a user to answer. The question should be compelling and the solution should be focused on helping users achieve their goals.
+
+- What is the price range & star distribution of different categories of restaurants in Philadelphia? 
+- Are there any differences between different categories?
+- Which category of restaurants is more common for a given price range & star?
 
 ## Design
 
 TODO: **A rationale for your design decisions.** How did you choose your particular visual encodings and interaction techniques? What alternatives did you consider and how did you arrive at your ultimate choices?
 
+To answer the questions above, we would like to design functions including:
+
+- Allow users to be able to filter according to restaurant categories in Philadelphia.
+- Show the selected restaurants on the map of Philadelphia
+- For a selected area, show the distribution of price range and stars of the restaurants
+- For a selected area, show the number of restaurants in different categories
+- For a selected price range and star, show the number of restaurants in different categories
+
+And we adopted
+
+- A stremlit multi-selection input widget
+- A folium nteractive map that allows users to zoom in/out
+- Tooltip on the map to show the name, address, star, and price range.
+- Altair charts that shows the statistics of the selected map area
+- Altair charts selection to allow users choose specific price range and stars
+
+Alternatives we considered
+
+- We would like to use the Altair map in the beginning as it is better to develop interactive features. However, it does not allow zooming in/out function, we turned to folium instead.
+- We tried to use an altair chart to filter folim map, but it is not feasible. As a result, we turned to streamlit input to filter the map.
+
+
 ## Development
 
 TODO: **An overview of your development process.** Describe how the work was split among the team members. Include a commentary on the development process, including answers to the following questions: Roughly how much time did you spend developing your application (in people-hours)? What aspects took the most time?
 
+
+- At first, we worked together on the Exploratory Data Analysis and decided on the dataset we would like to work on, the data we need, and interaction techniques we want to include in our app.
+- We split the work to data cleaning and interaction implementation among 2 of us. When encountering problems, we would schedule a meeting to discuss the blocks together.
+- The data cleaning took about 13 hours. The interaction implementaton took about 12 hours.
+- Time-consuming aspects: explore the data, find the right interaction tools
+
 ## Success Story
 
 TODO:  **A success story of your project.** Describe an insight or discovery you gain with your application that relates to the goals of your project.
+
+- In Philadelphia, bars are significantly more than other restaurants. And the price range of bars are relatively higher. Also, bars usually have good stars.
